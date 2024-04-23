71bd859cd0e33992e008d0149a75fba4f0566fb4
AlessandroBernardoEusebio <u202113640@upc.edu.pe>
2024-04-23 15:44:43 -0500

    chore: Organizar estructura del proyecto

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import router from '@/router.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.use(router)
+
+app.component('pv-button', Button)
+app.component('pv-input-text', InputText)
+
+app.mount('#app')
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import {createRouter,createWebHistory} from 'vue-router'
+
+const routes = [
+  // Coloquen los paths de sus componentes aquí
+
+
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
