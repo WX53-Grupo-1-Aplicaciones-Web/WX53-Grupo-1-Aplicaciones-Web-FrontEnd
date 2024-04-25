fadb2022e4a7905ad58de91fcb96aff3574389f5
AlessandroBernardoEusebio <u202113640@upc.edu.pe>
2024-04-25 15:53:43 -0500

    feat: Agregar v1 Detalle del producto (Sin API)

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import {createRouter,createWebHistory} from 'vue-router'
 import TheCatalog from '@/views/the-catalog.component.vue'
+import TheProductDetail from '@/views/product-detail.component.vue'
 
 const routes = [
   // Coloquen los paths de sus componentes aquí
-  { path: '/catalog', component: TheCatalog }
+  { path: '/catalog', component: TheCatalog },
+  { path: '/product_detail', component: TheProductDetail }
 ]
 
 const router = createRouter({
--- src/views/product-detail.component.vue
+++ src/views/product-detail.component.vue
@@ -1,11 +1,85 @@
-<script setup>
-
-</script>
-
 <template>
+  <div class="product-detail">
+    <AppToolbar />
+    <SearchBar />
 
+    <div class="product-detail-content">
+      <!-- Imagen del producto -->
+      <div class="product-image">
+        <img src="../images/mateBurilado.png" alt="Imagen del producto">
+      </div>
+
+      <!-- Detalles del producto -->
+      <div class="product-info">
+        <!-- Información del autor -->
+        <div class="author-info">
+          <h2>Hecho por <span class="author-name">Leonidas Orellana  </span></h2>
+          <div class="author-details">
+            <img class="author-image" src="../images/craftermanProfile.png" alt="Imagen del autor">
+          </div>
+        </div>
+        <p class="author-description">Leonidas Orellana de Ayacucho nos ofrece su colección de estatuillas inspirados en los héroes y precursores de la independencia del Perú.</p>
+        <!-- Detalles del producto -->
+        <div class="product-details">
+          <h3>Detalles del producto</h3>
+          <ul>
+            <li>Estilo: Escultura tradicional cuzqueña</li>
+            <li>Material: Piedra de andesita</li>
+            <li>Técnica: Talla a mano</li>
+            <li>Dimensiones: 30cm de altura, 20cm de ancho y 15cm de profundidad</li>
+            <li>Coloración: Tonalidades tierra y toques dorados</li>
+            <li>Características destacadas:</li>
+            <!-- Agrega más detalles según sea necesario -->
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import AppToolbar from '@/components/the-application-toolbar.component.vue'
+import SearchBar from '@/components/the-product-searching.component.vue'
+export default {
+  name: 'TheProductDetail',
+  components: {
+    AppToolbar,
+    SearchBar,
+  }
+}
+</script>
 
+<style scoped>
+.product-detail-content {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+}
+
+.product-image {
+  width: 50%;
+}
+
+.product-info {
+  width: 45%;
+}
+
+.author-info {
+  display: flex;
+  align-items: center; /* Alinea los elementos verticalmente */
+}
+
+.author-name {
+  margin-right: 10px; /* Ajusta el margen derecho */
+}
+
+.author-details {
+  display: flex;
+  align-items: center;
+}
+
+.author-image {
+  width: 50px;
+  margin-right: 10px; /* Ajusta el margen derecho */
+}
 </style>
