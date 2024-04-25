fc14a88c0ce1fda67f212ab279623ea61f82dc0c
AlessandroBernardoEusebio <u202113640@upc.edu.pe>
2024-04-25 15:08:01 -0500

    feat: Agregar productos al catalogo de vista falsa (sin API)

---
+++ src/components/product-catalog.component.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  name: 'ProductCatalog'
+}
+</script>
+
+<template>
+  <div class="product">
+    <img class="product-image" src="../images/mateBurilado.png" alt="Product Image">
+    <div class="product-info">
+      <h2 class="product-name">Nombre del Producto</h2>
+      <p class="product-description">Descripción del producto...</p>
+      <div class="product-price">Precio: $100</div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.product {
+  display: flex;
+  position: relative; /* Asegúrate de que .product tenga una posición relativa */
+  margin-bottom: 20px;
+  border-bottom: 1px solid #9b9b9b;
+}
+
+.product-image {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-bottom:20px;
+  border: 1px solid #000;
+}
+
+.product-info {
+  margin-left: 20px;
+}
+
+.product-name {
+  font-weight: bold;
+}
+
+.product-description {
+  margin-top: 10px;
+}
+
+.product-price {
+  position: absolute; /* Hace que el precio sea absoluto */
+  bottom: 10px; /* Posiciona el precio en la parte inferior */
+  right: 10px; /* Posiciona el precio a la derecha */
+  padding: 10px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  width: fit-content;
+  background-color: #449AFF;
+}
+</style>
--- src/views/the-catalog.component.vue
+++ src/views/the-catalog.component.vue
@@ -2,22 +2,75 @@
   <div>
     <AppToolbar></AppToolbar>
     <SearchBar></SearchBar>
+
+  </div>
+  <div class="catalog-container">
+    <!-- División izquierda con categorías -->
+    <div class="categories">
+      <h2>Categorías</h2>
+      <div><span>Joyería artesanal</span></div>
+      <div><span>Textiles y tejidos</span></div>
+      <div><span>Cerámica y alfarería</span></div>
+      <div><span>Artesanía en madera</span></div>
+      <div><span>Artesanía en cuero</span></div>
+      <div><span>Arte visual</span></div>
+      <div><span>Cuidado personal</span></div>
+      <div><span>Juguetes</span></div>
+      <div><span>Decoración del hogar</span></div>
+      <div><span>Productos gastronómicos artesanales</span></div>
+      <!-- Aquí puedes agregar tus categorías -->
+    </div>
+
+    <!-- División derecha con productos -->
+    <div class="products">
+      <ProductCatalog></ProductCatalog>
+      <ProductCatalog></ProductCatalog>
+    </div>
   </div>
 </template>
 
 <script>
 import AppToolbar from '@/components/the-application-toolbar.component.vue'
 import SearchBar from '@/components/the-product-searching.component.vue'
+import ProductCatalog from '@/components/product-catalog.component.vue'
 export default {
   name: 'TheCatalog',
   components: {
+    ProductCatalog,
     AppToolbar,
-    SearchBar
+    SearchBar,
   }
 }
 
 </script>
 
 <style scoped>
+.catalog-container {
+  display: flex;
+  min-height: 100vh; /* Establece la altura mínima como 100% del viewport */
 
+}
+
+.categories {
+  width: 400px;
+  padding: 20px;
+  background-color: #E0EDFF;
+  min-height: 100%; /* Ajusta la altura al 100% del contenedor principal */
+  border-right: 1px solid black;
+  overflow-y: auto; /* Añade un desplazamiento vertical si el contenido excede la altura */
+
+}
+
+.categories h2 {
+  color: #000;
+  margin-left: 20px;
+}
+
+.products {
+  flex-grow: 1; /* El contenido ocupa todo el espacio restante */
+}
+.categories div {
+  margin-left: 20px;
+  margin-bottom: 10px; /* Ajusta el valor según la separación deseada */
+}
 </style>
