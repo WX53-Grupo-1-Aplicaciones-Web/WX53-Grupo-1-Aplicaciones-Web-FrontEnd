ddf60faf14381496a09e56b6eb6bc02c42b37a99
Mrkonik16 <164562584+Mrkonik16@users.noreply.github.com>
2024-04-25 22:35:41 -0500

    Create toolbar-login.component.vue
    feat: integracion de vistas. Creacion del footer del login

---
+++ src/components/toolbar-login.component.vue
@@ -0,0 +1,40 @@
+<script>
+</script>
+<template>
+  <div class="toolbar">
+    <img src="../images/logoArtisania.png" alt="Logo" class="logo">
+
+    <button type="login">Iniciar sesión</button>
+
+    <button type="register">Registrarse</button>
+  </div>
+</template>
+
+<style scoped>
+.toolbar {
+  width: 100%;
+  min-height: 85px;
+  background-color: #002C3E;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+}
+
+button {
+  padding: 12px 20px;
+  background-color: #002C3E;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-top: 10px;
+  border-radius: 20px;
+  position: relative;
+  left: 1100px;
+  font-size: 20px
+}
+
+.logo {
+  max-height: 65px;
+  max-width: 100%;
+}
+</style>
