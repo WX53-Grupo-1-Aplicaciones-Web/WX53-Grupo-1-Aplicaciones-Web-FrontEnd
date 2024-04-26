e80ef0be9f7270c80408025ca97d129a9b7a019c
Mrkonik16 <164562584+Mrkonik16@users.noreply.github.com>
2024-04-25 22:40:33 -0500

    Create the-register.component.vue
    feat: integracion de vistas. Pantalla de registro de usuario

---
+++ src/views/the-register.component.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <LoginToolbar></LoginToolbar>
+  </div>
+
+  <div class="login-container">
+    <div class="login-wrapper">
+      <div class="login-form">
+        <div class="login-header">
+          <h2>Crea tu cuenta</h2>
+        </div>
+
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label for="email">Correo electrónico:</label>
+            <input type="email" id="email" v-model="email" required />
+          </div>
+
+          <div class="form-group">
+            <label for="user">Usuario:</label>
+            <input type="text" id="user" v-model="user" required />
+          </div>
+
+          <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" v-model="password" required />
+          </div>
+
+          <div class="form-group">
+            <label for="password"> Confirmar contraseña:</label>
+            <input type="password" id="password" v-model="password" required />
+          </div>
+
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import LoginToolbar from '@/components/toolbar-login.component.vue'
+import Footer from '@/components/the-footer.component.vue'
+export default {
+  name: 'TheRegister',
+  components: {
+    LoginToolbar,
+    Footer,
+  }
+}
+
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  background-color: white;
+}
+
+.login-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.login-form {
+  background-color: lightblue;
+  padding: 20px;
+  border-radius: 20px;
+  width: 700px;
+}
+
+.login-header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.login-header h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.login-header p {
+  font-size: 14px;
+  color: #666;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  width: 100%;
+  border-radius: 3px;
+}
+
+button {
+  padding: 12px 20px;
+  background-color: white;
+  color: black;
+  border: none;
+  cursor: pointer;
+  margin-top: 10px;
+  border-radius: 50px;
+}
+</style>
