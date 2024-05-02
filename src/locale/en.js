611456fbfd2a6d92747b49983619015266086b07
Mrkonik16 <164562584+Mrkonik16@users.noreply.github.com>
2024-05-02 12:21:16 -0500

    feat: Agregar convencion de idioma

---
+++ src/locale/en.js
@@ -0,0 +1,41 @@
+const en = {
+  login: {
+    title: 'Login',
+    welcome: 'Welcome to ARTISANIA',
+    emailLabel: 'Email:',
+    passwordLabel: 'Password:',
+    signInButton: 'Sign-in',
+    noAccount: 'Don\'t have an account?',
+    registerLink: 'Register',
+    forgotPasswordLink: 'Forgot your password?'
+  },
+
+  loginToolbar:{
+    login: 'Log-in',
+    register: 'Sign-in'
+  },
+
+  footer: {
+    slogan1: 'Connecting all your hearts with',
+    slogan2: 'handmade art'
+  },
+
+  register: {
+    title: 'Create you Account',
+    email: 'Email:',
+    user: 'User:',
+    password: 'Password:',
+    Cpassword: 'Confirm password:',
+    register: 'Sign-in'
+  },
+
+  passchange: {
+    title: 'Reset your password',
+    user: 'User:',
+    password: 'Password:',
+    confirm: 'Confirm new Password:',
+    reset: 'Reset Password'
+  }
+}
+
+export default en
